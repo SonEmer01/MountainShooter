c4158e36dd1ebc9650d0790c44f0b6d51ebad0ae
SonEmer01 <emerson.florianorad1@gmail.com>
2026-03-12 23:32:58 -0300

    Adicionando as classes vindas do StarUML

---
+++ code/level.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+class Level:
+    def __init__(self):
+        self.window = None
+        self.name = None
+        self.entity_list = None
+
+    def run(self, ):
+        pass
+
+    def entity_list(self, ):
+        pass

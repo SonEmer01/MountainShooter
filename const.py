3d04c4f447a2a27870d0c6effeb1d60ace1b3e6d
SonEmer01 <emerson.florianorad1@gmail.com>
2026-03-16 20:58:20 -0300

    Criação do Menu e inserindo os Assets, background e sons

---
+++ const.py
@@ -0,0 +1,16 @@
+
+# Template
+WIN_WIDTH = 576
+WIN_HEIGHT = 324
+
+# Cores
+
+COLOR_ORANGE = (255, 128, 0)
+COLOR_WHITE = (255, 255, 255)
+
+# M
+MENU_OPTION = ['NOVO JOGO 1P',
+               'NOVO JOGO 2P - COOPERATIVO',
+               'NOVO JOGO 2P - COMPETITIVO',
+               'PONTUAÇÃO',
+               'SAIR DO JOGO']
